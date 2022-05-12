552756d9a17c07b625a4edcccdd9c6707c9950e5
ChabaJosa <chabagjg@gmail.com>
2022-05-12 17:40:39 -0400

    Added Tachyons but the npm package didn't work I had to use the link tag

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
